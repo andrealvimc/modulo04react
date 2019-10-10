6d4a6a7ed2f02566ce5a526daf170809a701ca42
Andre Alvim <andre@alvim.net>
2019-10-10 19:11:56 -0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+﻿const path = require("path");
+
+module.exports = {
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
